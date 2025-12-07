14779177352ec318ff56f7b1165373015dceaf62
Vaibhav Katariya <86615124+VaibhavKatariya@users.noreply.github.com>
2025-12-07 23:21:00 +0530

    fix: fux contact us ui

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -110,7 +110,7 @@ export default function ProjectPage() {
           ) : (
             <>
               <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
-                <div className="h-16">
+                <div className="h-16 hidden">
                   <input
                     type="text"
                     name="honeypot"
